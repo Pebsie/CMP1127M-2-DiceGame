e635eb5437b7cc4f12f5c410ff26aedef46b0b5e
Thomas Lock <email@peb.si>
2017-05-02 19:14:06 +0100

    add player functions

--- Program.cs
+++ Program.cs
@@ -1,36 +1,56 @@
 using System;
+using System.Collections.Generic; //to allow for lists
 
 namespace CMP1127M {
 
     class Program {
 
         public class Game {
-            Player[] players;
-            int maxScore = 50;
-            int turn = 0;
+            static Player[] players;
+            static int maxScore = 50;
+            static int turn = 0;
+
+            public Game(int playerCount) {
+
+              players = new Player[playerCount];
+              for (int i = 0; i < playerCount; i++) {
+                players[i] = new Player();
+              }
+
+            }
+
+            public void nextTurn() {
+              players[turn].turn();
+              turn++;
+              if (turn > players.Length) {
+                turn = 0;
+              }
+            }
         }
 
         public class Die {
-            int value;
-            Random rnd = new Random()
+            static int faceValue;
+            static Random rnd = new Random();
 
             public Die() {
             }
 
-            public static void roll() {
-                value = rnd.Next(1, 6);
+            public void roll() {
+                faceValue = rnd.Next(1, 6);
+                Console.Write(faceValue);
             }
 
             public int getValue() {
-                return value;
+                return faceValue;
             }
         }
 
         public class Player {
-            string name;
-            int score = 0;
-            
-            
+            static string name;
+            static int score = 0;
+
+            public List<int> previousRolls = new List<int>(); //this is a list so that we can store a potentially infinite set of values
+
             Die[] dice = new Die[4];
 
             public Player(string name = "Guest") {
@@ -42,12 +62,32 @@ namespace CMP1127M {
             }
 
             public int turn() {
+
                 Console.WriteLine("Your turn, " + name + ".\nScore: " + score + "(1) - Throw, reroll three\n(2) - Throw All, double points");
+                char option = Console.ReadKey().KeyChar;
+
+                if (option == '1') {
+                  Console.WriteLine("\nPress any key to roll!");
+                  for (int i = 0; i < 5; i++) {
+                    Console.ReadKey();
+                    dice[i].roll();
+                    Console.Write(dice[i].getValue() + " ");
+                  }
+                }
+
+                return 0;
             }
         }
 
         static void Main(string[] args) {
-
+          Game game = new Game(3);
+          game.nextTurn();
+          game.nextTurn();
+          game.nextTurn();
+          game.nextTurn();
+          game.nextTurn();
+          game.nextTurn();
+          game.nextTurn();
         }
 
     }
